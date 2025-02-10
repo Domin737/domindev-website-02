2597b12a8b4014ef6e3b15cc0cbf4d6297707b47
Domin737 <p.dominiak.pd@gmail.com>
2025-02-11 00:01:26 +0100

    Update style FloatingChat

--- src/components/layout/FloatingChat.tsx
+++ src/components/layout/FloatingChat.tsx
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 import axios from "axios";
-import { FaComments, FaTimes } from "react-icons/fa";
+import { BiMessageDetail } from "react-icons/bi";
 import "./FloatingChat.scss";
 
 interface ChatResponse {
@@ -57,7 +57,22 @@ const FloatingChat = () => {
   return (
     <div className="floating-chat">
       <button className="floating-chat-toggle" onClick={toggleChat}>
-        {isOpen ? <FaTimes /> : <FaComments />}
+        {isOpen ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <line x1="18" y1="6" x2="6" y2="18" />
+            <line x1="6" y1="6" x2="18" y2="18" />
+          </svg>
+        ) : (
+          <BiMessageDetail />
+        )}
       </button>
 
       {isOpen && (
